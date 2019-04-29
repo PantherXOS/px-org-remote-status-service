8780a45df733afe5bbb25f7cde6153bd875f5a57
f.sajadi <f.sajadi@pantherx.org>
2019-04-29 19:06:40 +0430

    create monitrc file with permisions

--- src/SystemStats.cpp
+++ src/SystemStats.cpp
@@ -108,7 +108,7 @@ void SystemStats::monitStatusParser(string status,StatsParam&  statParam) {
 
 StatsParam SystemStats::get() {
     StatsParam statsParam;
-    string command = "monit -c etc/monitrc status";
+    string command = "monit -c "+string(getpwuid(getuid())->pw_dir)+"/monitrc status";
     string result = UTILS::COMMAND::Execute(command.c_str());
     cout<<"(monit -c etc/monitrc status) run successfully " << result <<endl;
     monitStatusParser(result, statsParam);
@@ -143,9 +143,21 @@ void SystemStats::diskStatusParser(string status, vector<DiskParams> &diskStatus
 }
 
 SystemStats::SystemStats() {
-    string command = "monit -c etc/monitrc";
-//    chmod((UTILS::PATH::getExecPath() + "monitrc").c_str(),S_IRWXU);
-//    cout << command << endl;
+    // read file from bin
+    string inFile = (UTILS::PATH::getExecPath() + "monitrc").c_str();
+    std::ifstream configFile(inFile);
+    std::string content( (std::istreambuf_iterator<char>(configFile) ),
+                         (std::istreambuf_iterator<char>()));
+
+    // save to ~/
+    ofstream outConfigFile;
+    outConfigFile.open ((string(getpwuid(getuid())->pw_dir) + "/monitrc"));
+    outConfigFile << content.c_str();
+    outConfigFile.close();
+
+    string command = "monit -c "+string(getpwuid(getuid())->pw_dir)+"/monitrc";
+    chmod((string(getpwuid(getuid())->pw_dir) + "/monitrc").c_str(),S_IRWXU);
+    cout << command << endl;
     string result = UTILS::COMMAND::Execute(command.c_str());
     cout<<"monit run successfully " << result <<endl;
 }
