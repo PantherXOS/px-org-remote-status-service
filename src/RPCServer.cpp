d1d8355b32b468cd7471ecde13e3e18d5ebb4849
Reza Alizadeh Majd <r.majd@pantherx.org>
2021-04-15 21:47:11 +0430

    fix performance issue of PRC thread
    Signed-off-by: Reza Alizadeh Majd <r.majd@pantherx.org>

--- src/RPCServer.cpp
+++ src/RPCServer.cpp
@@ -46,7 +46,7 @@ void RPCServer::start() {
 
             while (instance->isRunning) {
                 waitScope.poll();
-                usleep(100);
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
             }
         }, this);
     }
